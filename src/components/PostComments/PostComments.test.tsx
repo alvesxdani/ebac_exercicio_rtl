dddbab0bae41e46f2b24e56a5a6b38d13f888dc2
alvesxdani <dani.alvesm22@gmail.com>
2024-05-02 00:22:19 -0300

    :test_tube: test: componente de postagem de comentários e componente de post

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,22 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from '@testing-library/react'
+import Post from '.'
+import PostComment from '.'
+import { debug } from 'console'
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+  it('Deve renderizar o componente corretamente', () => {
+    render(<PostComment />)
+    expect(screen.getByText('Comentar')).toBeInTheDocument()
+  })
+  test('Deve comentar duas vezes', () => {
+    const comments = [{ comment: 'teste 1' }, { comment: 'teste 2' }]
+    render(<Post />)
+    comments.map(({ comment }) => {
+      fireEvent.change(screen.getByTestId('input-comment'), {
+        target: { value: comment },
+      })
+      fireEvent.click(screen.getByText('Comentar'))
+    })
+    expect(screen.getByText('teste 1')).toBeInTheDocument()
+  })
+})
